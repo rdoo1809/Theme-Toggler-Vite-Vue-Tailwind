063628345e8a8a2480aa8b47f030cf52aa032ee9
Ryan Dooley <rdooley@conestogac.on.ca>
2025-03-14 13:06:49 -0400

    set up toggler switch instead of button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {useDark, useToggle} from "@vueuse/core";
 import NavBar from "@/views/NavBar.vue";
+import ThemeToggler from "@/views/ThemeToggler.vue";
 
 const isDark = useDark();
 const toggleDark = useToggle(isDark);
@@ -9,20 +10,26 @@ const toggleDark = useToggle(isDark);
 <template>
   <NavBar/>
 
+  <div class="max-w-full flex justify-center">
 
-    <div
-      class=" flex items-start justify-center shadow-md p-6 rounded-md bg-white dark:bg-slate-700 mt-20"
-    >
-      <div class="flex items-start sm:items-center mb-4">
-        <h1 class="text-4xl flex-1 dark:text-white">
-          Vue Tailwind Dark/Light Mode
+    <div class="max-w-7xl flex items-start justify-center shadow-md p-6 rounded-md bg-white dark:bg-black mt-20">
+
+      <div class="flex flex-col sm:items-center mb-4">
+        <h1 class="text-4xl dark:text-white">
+          Vue + Vite + Tailwind Dark/Light Mode Toggler
         </h1>
+        <p class="dark:text-white my-4">This is a template application to be used as a starting point for a new front end.</p>
+        <p class="dark:text-white">Comes equipped with a manual light/dark mode toggle switch, and hamburger nav for mobile display.</p>
       </div>
-      <button
-        @click="toggleDark()"
-        class="py-2 px-4 bg-black text-white rounded-md dark:bg-white dark:text-black"
-      >
-       Toggle {{ isDark ? "Light" : "Dark" }}
-      </button>
     </div>
+
+  </div>
+
+  <ThemeToggler/>
 </template>
+
+<style>
+
+</style>
+
+
---
+++ src/views/ThemeToggler.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <div class="absolute bottom-0 right-0 m-4">
+
+    <label class="relative inline-flex items-center cursor-pointer">
+
+      <input type="checkbox" class="sr-only peer" @click="toggleDark()" />
+
+      <div
+          class="w-24 h-12 bg-gray-300 peer-focus:outline-none peer-focus:ring-2
+             dark:bg-gray-700 rounded-full peer peer-checked:after:translate-x-12
+             peer-checked:after:border-white after:content-[''] after:absolute
+             after:top-1 after:left-1 after:w-10 after:h-10 after:bg-white after:border-gray-300
+             after:border after:rounded-full after:transition-all peer-checked:bg-grey-700"
+      >
+
+      </div>
+    </label>
+  </div>
+</template>
